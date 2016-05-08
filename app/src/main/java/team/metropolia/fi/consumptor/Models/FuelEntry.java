f547ad25c8a4099dc620dbf994ae8bde1d0984ac
Roma Laitarenko <rlaitarenko@iconnectpos.com>
2016-05-08 11:49:39 +0300

    Removed commented out and unused code.

--- app/src/main/java/team/metropolia/fi/consumptor/Models/FuelEntry.java
+++ app/src/main/java/team/metropolia/fi/consumptor/Models/FuelEntry.java
@@ -32,43 +32,16 @@ public class FuelEntry extends Model {
 
     public static int getAverageConsumption() {
 
-//        List<FuelEntry> entries = new Select().from(FuelEntry.class)
-//                .orderBy("createdOn DESC")
-//                .execute();
-//
-//        if (entries.size() < 2) {
-//            return 0;
-//        }
-//
-//        FuelEntry firstEntry = entries.get(0);
-//        FuelEntry lastEntry = entries.get(entries.size() - 1);
-//        float distance = lastEntry.odometer - firstEntry.odometer;
-//        float fuelUsed = calculateFuelUsed(entries);
-
         return Settings.calculateAverage();
     }
 
     public static int getMaxConsumption() {
 
-//        List<Integer> individualConsumptions = getConsumptionSlices();
-//
-//        if (individualConsumptions.isEmpty()) {
-//            return 0;
-//        }
-//
-//        return individualConsumptions.get(individualConsumptions.size() - 1);
         return Settings.retrieve_maxConsumption();
     }
 
     public static int getMinConsumption() {
 
-//        List<Integer> individualConsumptions = getConsumptionSlices();
-//
-//        if (individualConsumptions.isEmpty()) {
-//            return 0;
-//        }
-//
-//        return individualConsumptions.get(0);
         return Settings.retrieve_minConsumption();
     }
 
@@ -90,48 +63,10 @@ public class FuelEntry extends Model {
         double distanceTravelled = lastEntry.odometer - firstEntry.odometer;
         double fuelUsed = lastEntry.fuel;
 
-        int newLastComsumption = (int) (100.0 * fuelUsed / distanceTravelled);
+        int newLastConsumption = (int) (100.0 * fuelUsed / distanceTravelled);
 
-        Settings.updateMinMaxWith(newLastComsumption);
+        Settings.updateMinMaxWith(newLastConsumption);
 
-        return newLastComsumption;
-    }
-
-    @NonNull
-    private static List<Integer> getConsumptionSlices() {
-
-        List<Integer> consumptions = new ArrayList<>();
-        List<FuelEntry> entries = new Select().from(FuelEntry.class)
-                .orderBy("createdOn DESC")
-                .execute();
-
-        if (entries.size() < 2) {
-            return consumptions;
-        }
-
-        for (int i = 0; i < entries.size() - 1; i++) {
-
-            FuelEntry current = entries.get(i);
-            FuelEntry next = entries.get(i + 1);
-            float distance = next.odometer - current.odometer;
-            float fuel = next.fuel + current.fuel;
-            Integer consumption = (int) ((distance / fuel) * 100);
-
-            consumptions.add(consumption);
-        }
-
-        Collections.sort(consumptions);
-
-        return consumptions;
-    }
-
-    private static float calculateFuelUsed(List<FuelEntry> entries) {
-
-        float fuelUsed = 0;
-
-        for (FuelEntry entry : entries) {
-            fuelUsed += entry.fuel;
-        }
-        return fuelUsed;
+        return newLastConsumption;
     }
 }
